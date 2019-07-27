4563c2bb746b3da7406ce26c31f64043a4d34587
Nate Chiles <nate.chiles@gmail.com>
2019-07-26 22:52:09 -0400

    remove y’all

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var dotenv			= require('dotenv').config(),
 	methodOveride   = require("method-override"),
 	port 			= process.env.PORT || 5000
 	
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true }); //live database for app
-// mongoose.connect(process.env.MONGO_DB_TESTING, { useNewUrlParser: true }); //local database for testing
+// mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true }); //live database for app
+mongoose.connect(process.env.MONGO_DB_TESTING, { useNewUrlParser: true }); //local database for testing
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); //reads a form's input and stores it as a javascript object accessible through req.body
@@ -403,7 +403,7 @@ app.post("/payment/partial", isLoggedIn, function(req, res){
         	console.log('archived expense created');
         }
 	});
-	res.redirect("/show_expenses")
+	// res.redirect("/show_expenses")
 });
 
 app.get("/header", isLoggedIn, function(req, res){
--- public/js/payment.js
+++ public/js/payment.js
@@ -132,72 +132,71 @@ $(function() {
 });
 
 //UPDATE VARIABLES WITH AJAX
-    $(() => {
-        const $form = $('#form')
+$(() => {
+    const $form = $('#form')
 
-        $form.on('submit', handleForm)
+    $form.on('submit', handleForm)
+    
+
+    function handleForm(e) {
+        e.preventDefault()
+        
+        const options = {
+            method: $form.attr('method'),
+            url: $form.attr('action'),
+            data: $form.serialize(), 
+            success: 
+                $(document).ready (function(){
+                
+                    effects1 = function(){
+                        $(".status-bar").load(location.href+" .status-bar>*",""); /* reload status bar */
+                        return $(".status-bar");
+                    }
+
+                    effects2 = function(){
+                        $(".status-bar-overlay").load(location.href+" .status-bar-overlay>*",""); /* reload status bar */
+                        return $(".status-bar-overlay");
+                    }
+
+                    effects3 = function(){
+                        $(".bg-overlay").fadeIn("fast").addClass("show-bg-overlay").delay(1200).fadeOut(1500); //background
+                        $(".overlay").fadeIn("fast").addClass("show-overlay").delay(1200).slideUp(800).fadeOut(1000); //words
+                        $(".status-bar-overlay").delay(1500).animate({"font-size":".5em"}).fadeOut(400).fadeIn().animate({"font-size":"2.5em"});
+                        return $(".hasEffects");
+                    }
+
+                    effects4 = function(){
+                        $('#form')[0].reset(); 
+                        $('html, body').animate({scrollTop:0}, 'fast')
+                    }
+
+                    $('body').off().on('submit','#form', function() {  
+                        runAnimations([effects1,effects2,effects3,effects4]);
+                    });
+
+                    runAnimations = function(functionArray) {
+                    //extract the first function        
+                    var func = functionArray.splice(0, 1);
+
+                    //run it. and wait till its finished 
+                    func[0]().promise().done(function() {
+
+                        //then call run animations again on remaining array
+                        if (functionArray.length > 0) runAnimations(functionArray);
+                    });
+
+                    }
+
+                })
+        }
         
 
-        function handleForm(e) {
-            e.preventDefault()
-            
-            const options = {
-                method: $form.attr('method'),
-                url: $form.attr('action'),
-                data: $form.serialize(), 
-                success: 
-                    $(document).ready (function(){
-                    
-
-                        effects1 = function(){
-                            $(".status-bar").load(location.href+" .status-bar>*",""); /* reload status bar */
-                            return $(".status-bar");
-                        }
-
-                        effects2 = function(){
-                            $(".status-bar-overlay").load(location.href+" .status-bar-overlay>*",""); /* reload status bar */
-                            return $(".status-bar-overlay");
-                        }
-
-                        effects3 = function(){
-                            $(".bg-overlay").fadeIn("fast").addClass("show-bg-overlay").delay(1200).fadeOut(1500); //background
-                            $(".overlay").fadeIn("fast").addClass("show-overlay").delay(1200).slideUp(800).fadeOut(1000); //words
-                            $(".status-bar-overlay").delay(1500).animate({"font-size":".5em"}).fadeOut(400).fadeIn().animate({"font-size":"2.5em"});
-                            return $(".hasEffects");
-                        }
-
-                        effects4 = function(){
-                            $('#form')[0].reset(); 
-                            $('html, body').animate({scrollTop:0}, 'fast')
-                        }
-
-                        $('body').off().on('submit','#form', function() {  
-                            runAnimations([effects1,effects2,effects3,effects4]);
-                        });
-
-                        runAnimations = function(functionArray) {
-                        //extract the first function        
-                        var func = functionArray.splice(0, 1);
-
-                        //run it. and wait till its finished 
-                        func[0]().promise().done(function() {
-
-                            //then call run animations again on remaining array
-                            if (functionArray.length > 0) runAnimations(functionArray);
-                        });
-
-                        }
-
-                    })
-            }
-            
-
-            $.ajax(options).done(response => {
-                console.log(response);
-                 
-            })
-        }    
-    })
+        $.ajax(options).done(response => {
+            console.log(response);
+                
+        })
+    }    
+})
 
 
     //PARTIAL
