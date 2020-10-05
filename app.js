06e6dfc813c642e7b7c4125b6cd0058910d3bdb8
Nate Chiles <nate.chiles@gmail.com>
2020-10-05 10:31:50 -0400

    Change to mongo Atlas

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var	express 	  	= require('express'),
 	MongoStore 		= require('connect-mongo')(session);
 	port 			= process.env.PORT || 5000
 	
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true }); //live database for app
+// mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true }); //live database for app
+mongoose.connect(process.env.MONGO_ATLAS, { useNewUrlParser: true }); //live database for app
 // mongoose.connect(process.env.MONGO_DB_TESTING, { useNewUrlParser: true }); //local database for testing
 
 app.use(bodyParser.urlencoded({extended: true}));
